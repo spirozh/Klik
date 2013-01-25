ac91f0cafcc57be21a7c8f26e95c2a67dee3991e
jason.songhurst <jason.songhurst@gmail.com>
2013-01-24 23:49:43 -0600

    change 'xp' to 'points'

--- src/spirozh/klik/Klik.java
+++ src/spirozh/klik/Klik.java
@@ -3,7 +3,6 @@ package spirozh.klik;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.view.View;
 import android.view.Window;
 import android.widget.ProgressBar;
@@ -12,104 +11,108 @@ import android.widget.TextView;
 public class Klik extends Activity {
 
 	private int _level;
-	private int _xp;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_klik);
+	private int _points;
 
-        View.OnLongClickListener lcl = new View.OnLongClickListener() {
-        	@Override
-        	public boolean onLongClick(View view) {
-        		reset();
-        		return true;
-        	}
-        };
-        
-        View.OnClickListener cl = new View.OnClickListener() {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_klik);
+
+		View parent = (View) findViewById(R.id.levelProgress).getParent();
+		parent.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				clicked();
 			}
-		};
+		});
 
-		View parent = (View) findViewById(R.id.levelProgress).getParent();
-        parent.setOnLongClickListener(lcl);
-        parent.setOnClickListener(cl);
-    }
-    
-    protected void onStart() {
-    	super.onStart();
-    	
-        // load _level and _xp
-    	SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-    	_xp = (int) sharedPref.getLong(getString(R.string.xp), 0);
-    	_level = (int) sharedPref.getLong(getString(R.string.level), 0);
-
-    	// prepare progress bar
-		((ProgressBar) findViewById(R.id.levelProgress)).setMax(xpForLevel(_level+1) - xpForLevel(_level));
-		
-		updateLabels();
-    }
-    
-    protected void onStop() {
-    	super.onStop();
-    	
-    	// save _level and _xp
-    	SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-    	SharedPreferences.Editor editor = sharedPref.edit();
-    	editor.putLong(getString(R.string.xp), _xp);
-    	editor.putLong(getString(R.string.level), _level);
-    	editor.commit();
-    }
-    
-    private void reset() {
-    	_xp = 0;
-    	_level = 0;
-    	
-    	ProgressBar pb = (ProgressBar) findViewById(R.id.levelProgress);
-    	pb.setMax(xpForLevel(1));
-
-    	updateLabels();
-    }
-    
-    public void clicked() {
-    	_xp++;
-    	
-    	if (_xp >= xpForLevel(_level + 1)) {
-    		_level++;
-    		((ProgressBar) findViewById(R.id.levelProgress)).setMax(xpForLevel(_level+1) - xpForLevel(_level));
-    	}
-    	
-    	updateLabels();
-    }
-    
-    static int xpForLevel(int level) {
-    	return fib(level + 6) - fib(6);
-    }
-    
-    static int fib(int x) {
-    	int a = 0;
-    	int b = 1;
-    	
-    	while (x > 0) {
-    		int t = b;
-    		b = a+b;
-    		a = t;
-    		x--;
-    	}
-    	
-    	return a;
-    }
-
-	private void updateLabels() {
-		((TextView) findViewById(R.id.experienceLabel)).setText(getString(R.string.xp_label_prefix) + + _xp);
-		((TextView) findViewById(R.id.levelLabel)).setText(getString(R.string.level_label_prefix) + _level);
-		
-		((ProgressBar) findViewById(R.id.levelProgress)).setProgress(_xp - xpForLevel(_level));
+		parent.setOnLongClickListener(new View.OnLongClickListener() {
+			@Override
+			public boolean onLongClick(View view) {
+				reset();
+				return true;
+			}
+		});
 	}
 
-    
+	protected void onStart() {
+		super.onStart();
+
+		// load _points
+		_points = (int) getPreferences(Context.MODE_PRIVATE).getLong(
+				getString(R.string.points), 0);
+
+		_level = levelForPoints(_points);
+
+		// prepare progress bar
+		((ProgressBar) findViewById(R.id.levelProgress))
+				.setMax(pointsForLevel(_level + 1) - pointsForLevel(_level));
+
+		updateLabels();
+	}
+
+	protected void onStop() {
+		super.onStop();
+
+		// save _level and _xp
+		getPreferences(Context.MODE_PRIVATE).edit()
+				.putLong(getString(R.string.points), _points).commit();
+	}
+
+	private void reset() {
+		_points = _level = 0;
+
+		updateProgressMax();
+		updateLabels();
+	}
+
+	public void clicked() {
+		if (_points++ >= pointsForLevel(_level + 1)) {
+			_level++;
+			updateProgressMax();
+		}
+
+		updateLabels();
+	}
+
+	static int pointsForLevel(int level) {
+		return fib(level + 6) - fib(6);
+	}
+
+	static int levelForPoints(int points) {
+		int l = 0;
+		while (points > pointsForLevel(l + 1))
+			l++;
+		return l;
+	}
+
+	static int fib(int x) {
+		int a = 0;
+		int b = 1;
+
+		while (x-- > 0) {
+			int t = b;
+			b = a + b;
+			a = t;
+		}
+
+		return a;
+	}
+
+	private void updateProgressMax() {
+		((ProgressBar) findViewById(R.id.levelProgress))
+				.setMax(pointsForLevel(_level + 1) - pointsForLevel(_level));
+	}
+
+	private void updateLabels() {
+		((TextView) findViewById(R.id.pointsLabel))
+				.setText(getString(R.string.points_prefix) + _points);
+		((TextView) findViewById(R.id.levelLabel))
+				.setText(getString(R.string.level_prefix) + _level);
+
+		((ProgressBar) findViewById(R.id.levelProgress)).setProgress(_points
+				- pointsForLevel(_level));
+	}
+
 }
